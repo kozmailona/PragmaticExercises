fca3da99b9a832a02bfb73989642d917938aa17e
kozmailona <kozmailona12@gmail.com>
2020-04-09 16:58:24 +0300

    added quote class and printing quotations

---
+++ src/main/java/inputprocessingoutput/PrintingQuotes.java
@@ -0,0 +1,15 @@
+package inputprocessingoutput;
+
+public class PrintingQuotes {
+    public static void main(String[] args) {
+
+        String s1 = "We should love one another.";
+        String s2 = "Well, who said this?";
+        String s3 = "Jesus";
+        String s4 = "Jesus says: ";
+        String s5 = "\"A new command I give you: Love one another. As I have loved you, so you must love one another" +
+                ".\"";
+
+        System.out.println(s1 + "\n" + s2 + " " + s3 + "\n" + s4 + s5 );
+    }
+}
