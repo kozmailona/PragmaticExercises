17c59b32c0a35332d2b9d3bd47630858ea288546
kozmailona <kozmailona12@gmail.com>
2020-04-09 19:23:38 +0300

    trying to print out string concatenations

--- src/main/java/inputprocessingoutput/MadLib.java
+++ src/main/java/inputprocessingoutput/MadLib.java
@@ -1,8 +1,20 @@
 package inputprocessingoutput;
 
+import java.util.Scanner;
+
 public class MadLib {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String input1 = scanner.nextLine();
+        String input2 = scanner.nextLine();
+        String input3 = scanner.nextLine();
+        String input4 = scanner.nextLine();
+        String noun = "Enter a noun: ";
+        String verb = "Enter a verb: ";
+        String adjective = "Enter an adjective: ";
+        String adverb = "Enter an adverb: ";
+//        System.out.println(noun + input1 + verb + input2 + adjective + input3 + adverb + input4);
 
     }
 }
