71abc856733620042322aeac60d45513448ac2fe
kozmailona <kozmailona12@gmail.com>
2020-04-09 15:48:40 +0300

    added first class and exercise

---
+++ src/main/java/inputprocessingoutput/SayingHello.java
@@ -0,0 +1,29 @@
+package inputprocessingoutput;
+
+import java.util.Scanner;
+
+public class SayingHello {
+
+        public static void main(String[] args) {
+
+            String myName = "Ilona";
+            System.out.println("What is your name? " + myName + "\n" + "Hello, " + myName + ", nice to meet you!");
+            System.out.println("----------");
+
+            System.out.println("What is your name? Brian \nHello, Ilona, nice to meet you!");
+
+
+
+            System.out.println("----------");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What is your name? ");
+            String name = scanner.nextLine();
+            System.out.println("Hello, " + name + ", nice to meet you!");
+            System.out.println("----------");
+
+
+
+        }
+    }
+
+
