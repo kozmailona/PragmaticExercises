1a1cf39f5825d0e6698c02f773518a5e1508b973
kozmailona <kozmailona12@gmail.com>
2020-04-09 17:37:39 +0300

    added madlib class and corrected quotes

---
+++ src/main/java/inputprocessingoutput/MadLib.java
@@ -0,0 +1,8 @@
+package inputprocessingoutput;
+
+public class MadLib {
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/main/java/inputprocessingoutput/PrintingQuotes.java
+++ src/main/java/inputprocessingoutput/PrintingQuotes.java
@@ -4,12 +4,15 @@ public class PrintingQuotes {
     public static void main(String[] args) {
 
         String s1 = "We should love one another.";
-        String s2 = "Well, who said this?";
+        String s2 = "Well, who said this? ";
         String s3 = "Jesus";
         String s4 = "Jesus says: ";
         String s5 = "\"A new command I give you: Love one another. As I have loved you, so you must love one another" +
                 ".\"";
+        String s6 = s1 + "\n" + s2 + s3 + "\n" + s4 + s5;
 
-        System.out.println(s1 + "\n" + s2 + " " + s3 + "\n" + s4 + s5 );
+        System.out.println(s6);
+
+//        System.out.println(s1 + "\n" + s2 + " " + s3 + "\n" + s4 + s5 );
     }
 }
