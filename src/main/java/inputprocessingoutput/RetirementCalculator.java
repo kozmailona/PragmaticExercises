4145ec2950d109f006ac0cd280aa62775d9ad4ff
kozmailona <kozmailona12@gmail.com>
2020-04-24 18:22:20 +0300

    trying to calculate retirement year

---
+++ src/main/java/inputprocessingoutput/RetirementCalculator.java
@@ -0,0 +1,28 @@
+package inputprocessingoutput;
+
+import jdk.vm.ci.meta.Local;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class RetirementCalculator {
+    public static void main(String[] args) {
+
+
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("What is your current age? ");
+//        String age = scanner.nextLine();
+//        System.out.println("At what age would you like to retire? ");
+//        String retireAge = scanner.nextLine();
+//
+//        int currentAge = Integer.parseInt(age);
+//        int retireAgeInt = Integer.parseInt(retireAge);
+
+
+
+
+    }
+}
