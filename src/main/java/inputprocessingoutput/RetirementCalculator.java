4ea95d718ee7d7c7f6c9ce9ddbe65f3380de3e5f
kozmailona <kozmailona12@gmail.com>
2020-05-14 01:27:17 +0300

    retirement calculator done

--- src/main/java/inputprocessingoutput/RetirementCalculator.java
+++ src/main/java/inputprocessingoutput/RetirementCalculator.java
@@ -1,28 +1,28 @@
 package inputprocessingoutput;
 
-import jdk.vm.ci.meta.Local;
-
-import java.time.LocalDate;
-import java.util.Calendar;
 import java.util.Scanner;
+
+import static java.util.Calendar.YEAR;
+import static java.util.Calendar.getInstance;
 
 public class RetirementCalculator {
     public static void main(String[] args) {
 
 
 
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print("What is your current age? ");
-//        String age = scanner.nextLine();
-//        System.out.println("At what age would you like to retire? ");
-//        String retireAge = scanner.nextLine();
-//
-//        int currentAge = Integer.parseInt(age);
-//        int retireAgeInt = Integer.parseInt(retireAge);
+        Scanner scanner = new Scanner(System.in);
 
-
-
+        System.out.print("What is your current age? ");
+        String age = scanner.nextLine();
+        System.out.print("At what age would you like to retire? ");
+        String retireAge = scanner.nextLine();
+        int currentYear = getInstance().get(YEAR);
+        int userCurrentAge = Integer.parseInt(age);
+        int wantedRetireAgeInt = Integer.parseInt(retireAge);
+        int yearsLeft = wantedRetireAgeInt - userCurrentAge;
+        System.out.println("You have " + yearsLeft + " years left until you can retire.");
+        int retireAgeInt = currentYear + yearsLeft;
+        System.out.println("It's " + currentYear + ", so you can retire in " + retireAgeInt + ".");
 
     }
 }
